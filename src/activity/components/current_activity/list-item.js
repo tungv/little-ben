f1be2786101fdb3dc8429ea9ab8458a808c6c87b
Tung <me@tungv.com>
2016-07-16 16:23:39 +0700

    render current_activity/list_item's times at a same time

--- src/activity/components/current_activity/list-item.js
+++ src/activity/components/current_activity/list-item.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { ListItem } from 'material-ui/List';
 import Time from 'react-time';
-import { mapProps } from 'recompose';
+import { mapProps, compose } from 'recompose';
 import * as COPY from '../../copy.json';
 import { interval } from '../../../utils/hoc/interval';
 
-const IntervalTime = interval(60e3)(Time);
-
 export const getPrimaryText = (session) => (
   <span>
-    <IntervalTime value={session.startTime} relative />
+    <Time value={session.startTime} relative />
     &nbsp;&mdash;&nbsp;
-    {session.endTime ? <IntervalTime value={session.endTime} relative /> : COPY.PRESENT_TIME}
+    {session.endTime ? <Time value={session.endTime} relative /> : COPY.PRESENT_TIME}
   </span>
 );
 
-const SessionListItem = mapProps(({ session }) => ({
-  primaryText: getPrimaryText(session),
-}))(ListItem);
+const SessionListItem = compose(
+  interval(60e3),
+  mapProps(({ session }) => ({
+    primaryText: getPrimaryText(session),
+  })),
+)(ListItem);
 
 export default SessionListItem;
