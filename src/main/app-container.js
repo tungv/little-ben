93291a28ae8d23696b582629e7e050d1f68e54f6
Tung <me@tungv.com>
2016-07-17 18:24:44 +0700

    add version/commit/build time to app

--- src/main/app-container.js
+++ src/main/app-container.js
@@ -1,14 +1,32 @@
 import React, { PropTypes } from 'react';
+import { compose, withHandlers, withState } from 'recompose';
 import { Provider as ReduxProvider } from 'react-redux';
-import AppBar from 'material-ui/AppBar';
 import { ConnectedActivityContainer } from '../activity';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const MainComponent = ({ store }) => (
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+import { version } from '../../package.json';
+
+const MainComponent = ({ store, showDrawer, onAppBarLeftMenuClick, onDrawerRequestChange }) => (
   <MuiThemeProvider>
     <ReduxProvider store={store}>
       <div>
-        <AppBar title="Little Ben" />
+        <AppBar
+          title="Little Ben"
+          onLeftIconButtonTouchTap={onAppBarLeftMenuClick}
+        />
+        <Drawer
+          open={showDrawer}
+          docked={false}
+          onRequestChange={onDrawerRequestChange}
+        >
+          <MenuItem>Version: {version}</MenuItem>
+          <MenuItem>Commit: {process.env.GIT_COMMIT.substr(0, 6) || 'dev'}</MenuItem>
+          <MenuItem>{process.env.BUILD_TIME}</MenuItem>
+        </Drawer>
         <ConnectedActivityContainer />
       </div>
     </ReduxProvider>
@@ -17,6 +35,19 @@ const MainComponent = ({ store }) => (
 
 MainComponent.propTypes = {
   store: PropTypes.object.isRequired,
+  showDrawer: PropTypes.bool.isRequired,
+  onAppBarLeftMenuClick: PropTypes.func.isRequired,
+  onDrawerRequestChange: PropTypes.func.isRequired,
 };
 
-export default MainComponent;
+const EnhancedMainComponent = compose(
+  withState('showDrawer', 'setShowDrawer', false),
+  withHandlers({
+    onAppBarLeftMenuClick: ({ setShowDrawer, showDrawer }) => () => {
+      setShowDrawer(!showDrawer);
+    },
+    onDrawerRequestChange: ({ setShowDrawer }) => open => setShowDrawer(open),
+  }),
+)(MainComponent);
+
+export default EnhancedMainComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var ClosureCompilerPlugin = require('webpack-closure-compiler');
-
+var moment = require('moment');
 var DEBUG = process.env.NODE_ENV !== 'production';
 
 var config = {
@@ -39,7 +39,9 @@ var config = {
   plugins: [
     new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(vi)$/),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.GIT_COMMIT': JSON.stringify(process.env.GIT_COMMIT || ''),
+      'process.env.BUILD_TIME': JSON.stringify(moment().format())
     })
   ]
 };
